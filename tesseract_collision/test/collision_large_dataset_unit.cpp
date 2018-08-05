67774437ef84b063b7d4fa218dc732cf325c9955
Levi <levi.armstrong@gmail.com>
2018-08-05 00:03:37 -0500

    Update large dataset unit test

--- tesseract_collision/test/collision_large_dataset_unit.cpp
+++ tesseract_collision/test/collision_large_dataset_unit.cpp
@@ -1,18 +1,16 @@
 
 #include "tesseract_collision/bullet/bullet_contact_checker.h"
+#include "tesseract_collision/fcl/fcl_contact_checker.h"
 #include <gtest/gtest.h>
 #include <ros/ros.h>
 
-TEST(TesseractCollisionLargeDataSetUnit, CollisionUnit)
+void runTest(tesseract::ContactCheckerBase& checker, bool use_convex_mesh = false)
 {
-  tesseract::BulletContactChecker checker;
-  sleep(10);
-
   // Add Meshed Sphere to checker
-  shapes::ShapePtr sphere(shapes::createMeshFromResource("package://tesseract_collision/test/sphere.stl"));
+  shapes::ShapePtr sphere(shapes::createMeshFromResource("package://tesseract_collision/test/sphere_p25m.stl"));
   double delta = 0.25;
 
-  std::size_t t = 2;
+  std::size_t t = 10;
   std::vector<std::string> link_names;
   tesseract::TransformMap location;
   for (std::size_t x = 0; x < t; ++x)
@@ -29,7 +27,11 @@ TEST(TesseractCollisionLargeDataSetUnit, CollisionUnit)
 
         obj3_shapes.push_back(shapes::ShapePtr(sphere->clone()));
         obj3_poses.push_back(sphere_pose);
-        obj3_types.push_back(tesseract::CollisionObjectType::ConvexHull);
+
+        if (use_convex_mesh)
+          obj3_types.push_back(tesseract::CollisionObjectType::ConvexHull);
+        else
+          obj3_types.push_back(tesseract::CollisionObjectType::UseShapeType);
 
         link_names.push_back("sphere_link_" + std::to_string(x) + std::to_string(y) + std::to_string(z));
 
@@ -44,31 +46,56 @@ TEST(TesseractCollisionLargeDataSetUnit, CollisionUnit)
   tesseract::ContactRequest req;
   req.link_names = link_names;
   req.contact_distance = 0.1;
-  req.type = tesseract::ContactRequestType::FIRST;
+  req.type = tesseract::ContactRequestType::ALL;
 
   tesseract::ContactResultMap result;
   tesseract::ContactResultVector result_vector;
+
+
+  ROS_ERROR_STREAM("Checking Collision");
+  ros::WallTime start_time = ros::WallTime::now();
   checker.calcCollisionsDiscrete(req, location, result);
   tesseract::moveContactResultsMapToContactResultsVector(result, result_vector);
+  ros::WallTime end_time = ros::WallTime::now();
+  ROS_ERROR_STREAM("DT: " << (end_time - start_time).toSec());
+
   ROS_ERROR_STREAM("Num Links: " << link_names.size());
   ROS_ERROR_STREAM("Contacts: " << result_vector.size());
 
-  for (const auto& link_name : link_names)
-  {
-    ROS_ERROR_STREAM("Link Name: " << link_name);
-  }
+//  for (const auto& link_name : link_names)
+//  {
+//    ROS_ERROR_STREAM("Link Name: " << link_name);
+//  }
 
-  for (const auto& contact_result : result_vector)
-  {
-    if (result.find(std::make_pair(contact_result.link_names[1], contact_result.link_names[0])) == result.end())
-    {
-      ROS_ERROR_STREAM("Duplicate Pair: [" << contact_result.link_names[0] << ", " << contact_result.link_names[1] << "]");
-    }
-  }
+//  for (const auto& contact_result : result_vector)
+//  {
+//    if (result.find(std::make_pair(contact_result.link_names[1], contact_result.link_names[0])) == result.end())
+//    {
+//      ROS_ERROR_STREAM("Duplicate Pair: [" << contact_result.link_names[0] << ", " << contact_result.link_names[1] << "]");
+//    }
+//  }
 
 
 //  checker.calcCollisionsContinuous(req, location, location2, result);
 //  tesseract::moveContactResultsMapToContactResultsVector(result, result_vector);
+}
+
+TEST(TesseractCollisionLargeDataSetUnit, FCLCollisionLargeDataSetUnit)
+{
+  tesseract::FCLContactChecker checker;
+  runTest(checker);
+}
+
+TEST(TesseractCollisionLargeDataSetUnit, FCLCollisionLargeDataSetConvexHullUnit)
+{
+  tesseract::FCLContactChecker checker;
+  runTest(checker, true);
+}
+
+TEST(TesseractCollisionLargeDataSetUnit, BulletCollisionLargeDataSetUnit)
+{
+  tesseract::BulletContactChecker checker;
+  runTest(checker);
 }
 
 int main(int argc, char** argv)
